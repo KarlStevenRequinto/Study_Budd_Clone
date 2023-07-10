b7e0a3da1b160dc271d7337f4bc936dc83fe49a2
karl.requinto <lerdrobott@gmail.com>
2023-07-10 08:46:28 +0800

    update room funcionality updated

--- studybud_clone/base/views.py
+++ studybud_clone/base/views.py
@@ -21,7 +21,7 @@ def home(request):
     room_count = rooms.count()
     room_messages = Message.objects.all().filter(Q(room__topic__name__icontains=q))
     context = {"rooms": rooms, "topics": topics,
-               "room_messages": room_messages,"room_count":room_count}
+               "room_messages": room_messages, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 
@@ -45,17 +45,19 @@ def room(request, pk):
 
 @login_required(login_url="login")
 def createRoom(request):
-    print("this is the request", request.user)
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == "POST":
-        # print(request.POST)
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {"form": form}
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get("name"),
+            description=request.POST.get("description")
+        )
+        return redirect('home')
+    context = {"form": form, "topics": topics}
     return render(request, "base/room_form.html", context)
 
 
@@ -63,17 +65,20 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse("You are not allowed here!")
 
     if request.method == "POST":
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get("name")
+        room.topic = topic
+        room.description = request.POST.get("description")
+        room.save()
+        return redirect('home')
 
-    context = {"form": form}
+    context = {"form": form, "topics": topics, "room":room}
     return render(request, 'base/room_form.html', context)
 
 
@@ -150,10 +155,12 @@ def deleteMessage(request, pk):
         return redirect("home")
     return render(request, "base/delete.html", {"obj": message})
 
-def userProfile(request,pk):
+
+def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
     room_messages = user.message_set.all()
     topics = Topic.objects.all()
-    context = {"user":user, "rooms":rooms,"room_messages":room_messages,"topics":topics}
-    return render(request,"base/profile.html",context)
+    context = {"user": user, "rooms": rooms,
+               "room_messages": room_messages, "topics": topics}
+    return render(request, "base/profile.html", context)
