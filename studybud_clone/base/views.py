232b204c62fb32dad05cc17e1acab53d37c452a6
karl.requinto <lerdrobott@gmail.com>
2023-07-12 03:26:43 +0800

    edit/update user account page

--- studybud_clone/base/views.py
+++ studybud_clone/base/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
@@ -78,7 +78,7 @@ def updateRoom(request, pk):
         room.save()
         return redirect('home')
 
-    context = {"form": form, "topics": topics, "room":room}
+    context = {"form": form, "topics": topics, "room": room}
     return render(request, 'base/room_form.html', context)
 
 
@@ -164,3 +164,16 @@ def userProfile(request, pk):
     context = {"user": user, "rooms": rooms,
                "room_messages": room_messages, "topics": topics}
     return render(request, "base/profile.html", context)
+
+
+@login_required(login_url="login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect("user-profile",pk=user.id)
+    return render(request, "base/update-user.html", {"form": form})
