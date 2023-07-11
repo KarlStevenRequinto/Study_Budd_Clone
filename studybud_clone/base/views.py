a7a16365470a095a2feb9b6e3c6debac81b39e77
karl.requinto <lerdrobott@gmail.com>
2023-07-12 05:03:22 +0800

    mobile responsiveness

--- studybud_clone/base/views.py
+++ studybud_clone/base/views.py
@@ -17,7 +17,7 @@ def home(request):
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) | Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.all().filter(Q(room__topic__name__icontains=q))
     context = {"rooms": rooms, "topics": topics,
@@ -177,3 +177,12 @@ def updateUser(request):
             form.save()
             return redirect("user-profile",pk=user.id)
     return render(request, "base/update-user.html", {"form": form})
+
+def topicsPage(request):
+    q = request.GET.get("q") if request.GET.get("q") != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request,"base/topics.html",{"topics":topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request,"base/activity.html",{"room_messages":room_messages})
