50088774b569e7e4864f4eb2ad84be551c896a11
karl.requinto <lerdrobott@gmail.com>
2023-07-15 15:37:19 +0800

    custom user model form

--- studybud_clone/base/views.py
+++ studybud_clone/base/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
@@ -66,6 +66,8 @@ def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
     topics = Topic.objects.all()
+    for topic in topics:
+        print(topic.name)
     if request.user != room.host:
         return HttpResponse("You are not allowed here!")
 
@@ -101,15 +103,15 @@ def loginPage(request):
         return redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username").lower()
+        email = request.POST.get("email").lower()
         password = request.POST.get("password")
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, "User does not exist")
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
@@ -127,10 +129,10 @@ def logoutUser(request):
 
 def registerPage(request):
     # page = "register"
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -172,17 +174,19 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == "POST":
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
-            return redirect("user-profile",pk=user.id)
+            return redirect("user-profile", pk=user.id)
     return render(request, "base/update-user.html", {"form": form})
+
 
 def topicsPage(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ''
     topics = Topic.objects.filter(name__icontains=q)
-    return render(request,"base/topics.html",{"topics":topics})
+    return render(request, "base/topics.html", {"topics": topics})
+
 
 def activityPage(request):
     room_messages = Message.objects.all()
-    return render(request,"base/activity.html",{"room_messages":room_messages})
+    return render(request, "base/activity.html", {"room_messages": room_messages})
